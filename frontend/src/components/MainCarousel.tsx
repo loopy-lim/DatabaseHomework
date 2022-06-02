2d760bb6c42da23088c7c91c0a9c7b28b599654f
loopy-lim <loopy@jnu.ac.kr>
2022-06-02 09:46:57 +0900

    feat: 이미지추가 및 캡션 추가를 한다

--- frontend/src/components/MainCarousel.tsx
+++ frontend/src/components/MainCarousel.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Carousel, CarouselItem } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 
 function MainCarousel() {
   const [index, setIndex] = React.useState(0);
@@ -7,29 +7,48 @@ function MainCarousel() {
   const handleSelect = (selectedIndex: number): void => {
     setIndex(selectedIndex);
   };
+
+  const img_css = {width: 1920, height: 720}
+  const caption_css = {background: "rgba(0, 0, 0, 0.6)", width: "100%", right: 0, left: 0, bottom: 0 }
+
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect} variant="dark">
-      <CarouselItem>
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
         <img
-          src="https://dummyimage.com/1980x1080/dadada/aaa"
+          src="/src/assets/images/jungle_img1.jpeg"
           alt="placeholer"
           className="d-block w-100"
+          style={img_css}
         />
-      </CarouselItem>
-      <CarouselItem>
+        <Carousel.Caption style={caption_css}>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
         <img
-          src="https://dummyimage.com/1980x1080/dadada/aaa"
+          src="/src/assets/images/jungle_img2.jpeg"
           alt="placeholer"
           className="d-block w-100"
+          style={img_css}
         />
-      </CarouselItem>
-      <CarouselItem>
+        <Carousel.Caption style={caption_css}>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
         <img
-          src="https://dummyimage.com/1980x1080/dadada/aaa"
+          src="/src/assets/images/jungle_img3.jpeg"
           alt="placeholer"
           className="d-block w-100"
+          style={img_css}
         />
-      </CarouselItem>
+        <Carousel.Caption style={caption_css}>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
     </Carousel>
   );
 }

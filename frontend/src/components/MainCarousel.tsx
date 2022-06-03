ab298c0c652ec23b1d16dbfe7a736ae000750e3b
loopy-lim <loopy@jnu.ac.kr>
2022-06-03 10:47:44 +0900

    fix: resize img

--- frontend/src/components/MainCarousel.tsx
+++ frontend/src/components/MainCarousel.tsx
@@ -8,17 +8,15 @@ function MainCarousel() {
     setIndex(selectedIndex);
   };
 
-  const img_css = {width: 1920, height: 720}
-  const caption_css = {background: "rgba(0, 0, 0, 0.6)", width: "100%", right: 0, left: 0, bottom: 0 }
+  const caption_css = {background: "rgba(0, 0, 0, 0.6)", width: "100%", right: 0, left: 0, bottom: 0 };
 
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
         <img
-          src="/src/assets/images/jungle_img1.jpeg"
+          src="/src/assets/images/jungle_img1.png"
           alt="placeholer"
           className="d-block w-100"
-          style={img_css}
         />
         <Carousel.Caption style={caption_css}>
           <h3>First slide label</h3>
@@ -27,10 +25,9 @@ function MainCarousel() {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          src="/src/assets/images/jungle_img2.jpeg"
+          src="/src/assets/images/jungle_img2.png"
           alt="placeholer"
           className="d-block w-100"
-          style={img_css}
         />
         <Carousel.Caption style={caption_css}>
           <h3>First slide label</h3>
@@ -39,10 +36,9 @@ function MainCarousel() {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          src="/src/assets/images/jungle_img3.jpeg"
+          src="/src/assets/images/jungle_img3.png"
           alt="placeholer"
           className="d-block w-100"
-          style={img_css}
         />
         <Carousel.Caption style={caption_css}>
           <h3>First slide label</h3>

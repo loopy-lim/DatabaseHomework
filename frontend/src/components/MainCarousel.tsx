60ac48efccd9dd9ca287c436ebf510e6ec0bbc42
loopy-lim <loopy@jnu.ac.kr>
2022-06-03 10:56:23 +0900

    feat: 내용을 추가한다

--- frontend/src/components/MainCarousel.tsx
+++ frontend/src/components/MainCarousel.tsx
@@ -19,8 +19,8 @@ function MainCarousel() {
           className="d-block w-100"
         />
         <Carousel.Caption style={caption_css}>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>Junggle</h3>
+          <p>정글은 생태계의 보고.<br/> 지구에서 육지 면적의 적은 부분만을 차지하지만, <br/>지구의 생명체의 1/3이 정글을기반으로 살아갑니다.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -30,11 +30,11 @@ function MainCarousel() {
           className="d-block w-100"
         />
         <Carousel.Caption style={caption_css}>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>새로운 시너지</h3>
+          <p>열대우림 속에서 다양한 종의 생물이 관계맺음 하듯,<br/> Junggle에서 다양한 배경과 특성을 가진 사람들이 어우러져 관계를 맺고,<br/> 새로운 생태계와 시너지를 형성했으면 하는 바람입니다.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      {/* <Carousel.Item>
         <img
           src="/src/assets/images/jungle_img3.png"
           alt="placeholer"
@@ -44,7 +44,7 @@ function MainCarousel() {
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
-      </Carousel.Item>
+      </Carousel.Item> */}
     </Carousel>
   );
 }

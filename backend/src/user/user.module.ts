dfcd7e347a83d426b1cc3ab999ade70573ea8971
loopy-lim <loopy@jnu.ac.kr>
2022-06-09 17:41:42 +0900

    feat: add user service and controller

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { AdminModule } from './admin/admin.module';
 import { ConsumerModule } from './consumer/consumer.module';
 import { ProviderModule } from './provider/provider.module';
+import { UserController } from './user.controller';
 
-@Module({ imports: [AdminModule, ConsumerModule, ProviderModule] })
+@Module({
+  imports: [AdminModule, ConsumerModule, ProviderModule],
+  controllers: [UserController],
+})
 export class UserModule {}

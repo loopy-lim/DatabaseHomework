76c04478207f896ae55c4af3f84426fb7e392284
loopy-lim <loopy@jnu.ac.kr>
2022-06-12 11:20:06 +0900

    feat: add exchange module, controller and service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { mysqlModule } from './database/mysql.config';
 import { UserModule } from './user/user.module';
+import { ExchangeModule } from './exchange/exchange.module';
 
 @Module({
-  imports: [mysqlModule, UserModule],
+  imports: [mysqlModule, UserModule, ExchangeModule],
   controllers: [AppController],
   providers: [AppService],
 })
